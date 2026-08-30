1e080a72817bfd05235091c5e338f3277fc756d1
zeal-daniela-fuentes <daniela.fuentes@zealitconsultants.ai>
2026-08-29 23:28:07 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/social-worker-check-in-workflow/sw.js', { scope: '/social-worker-check-in-workflow/' })})}
