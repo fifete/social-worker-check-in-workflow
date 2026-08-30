11cf41d834fdd8dec4411437c1e66b0063a7adb0
zeal-daniela-fuentes <daniela.fuentes@zealitconsultants.ai>
2026-08-29 22:59:02 -0500

    chore: deployment settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/social-worker-check-in-workflow/',
   plugins: [
     react(),
     tailwindcss(),
@@ -15,7 +16,20 @@ export default defineConfig({
         theme_color: '#FFFFFF',
         background_color: '#FFFFFF',
         display: 'standalone',
-        orientation: 'portrait'
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'attendance_192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'attendance_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
       }
     })
   ]
